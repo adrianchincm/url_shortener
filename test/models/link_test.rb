a0dd7324f84510f6dbcaf31059dd1638348b8eb3
Adrian Chin <syncing95@gmail.com>
2021-11-10 23:34:50 +0800

    add model and controller test

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -3,7 +3,33 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @link = Link.new(target_url: 'https://www.coingecko.com')
+  end
+
+  test 'link should be valid' do
+    assert @link.valid?
+  end
+
+  test 'target url should be present' do
+    @link.target_url = ''
+    assert_raise(Errno::ECONNREFUSED) do
+      @link.save
+    end
+  end
+
+  test 'target url should be in proper format' do
+    @link.target_url = 'coingecko.com'
+    assert_raise(Errno::ECONNREFUSED) do
+      @link.save
+    end
+  end
+
+  test 'target url should save successfully when in proper format' do
+    @link.save
+    assert_not_empty @link.target_url
+    assert_not_empty @link.short_url_slug
+    assert_not_empty @link.title
+    assert_equal(0, @link.clicks)
+  end
 end
