c0adaa41f0987c9eb65b288045a53a14e884113e
Adrian Chin <syncing95@gmail.com>
2021-11-10 22:20:24 +0800

    create link controller, added host url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 class Link < ApplicationRecord
   validates_presence_of :target_url
-  validates :target_url, format: URI::regexp(%w[http https])
+  validates :target_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
   validates_uniqueness_of :short_url_slug
   before_validation :generate_short_url
-  
+
   def generate_short_url
     self.short_url_slug = SecureRandom.alphanumeric(8)
     true
   end
 
   def short_url
-    Rails.application.routes.url_helpers.short_url(slug: self.short_url_slug)
+    Rails.application.routes.url_helpers.short_url(slug: short_url_slug)
   end
-  
 end
