6e5f1e790ea54662f795caee6bbbf2403561c5e5
Adrian Chin <syncing95@gmail.com>
2021-11-11 22:58:13 +0800

    added form component to create link model

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,4 +6,10 @@ class LinksController < ApplicationController
     link = Link.find_by(short_url_slug: slug)
     redirect_to link.target_url
   end
+
+  def create
+    puts "CREATING LINK"
+    link = Link.create(target_url: params[:target_url])
+    redirect_to link.target_url
+  end
 end
