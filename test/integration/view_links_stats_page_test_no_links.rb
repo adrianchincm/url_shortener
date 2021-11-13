903e6b7911b86d41aa0400e6ab08d8c5b3fb2729
Adrian Chin <syncing95@gmail.com>
2021-11-13 22:24:18 +0800

    run rubocop

--- test/integration/view_links_stats_page_test_no_links.rb
+++ test/integration/view_links_stats_page_test_no_links.rb
@@ -3,19 +3,18 @@
 require 'test_helper'
 
 class ViewLinkStatsPageTest < ActionDispatch::IntegrationTest
-
   setup do
     Link.delete_all
   end
 
   test 'view link stats page with no links generated' do
-    get "/links"
+    get '/links'
 
-    assert_response :success    
+    assert_response :success
 
     # assert link stats page with no links generated
     assert_select 'h2', 'Stats'
     assert_select 'p', 'Check your shortened links\' performance!'
-    assert_select 'td', 'There\'s no shortened links generated yet'    
+    assert_select 'td', 'There\'s no shortened links generated yet'
   end
 end
